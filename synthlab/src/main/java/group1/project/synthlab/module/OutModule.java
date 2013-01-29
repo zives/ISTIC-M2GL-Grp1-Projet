f32592dd697d24e35ac8e19b4207515eda605390
zives <d.zives@gmail.com>
2013-01-29 17:15:06 +0100

    module out prpduit un son

--- synthlab/src/main/java/group1/project/synthlab/module/OutModule.java
+++ synthlab/src/main/java/group1/project/synthlab/module/OutModule.java
@@ -6,6 +6,7 @@ import group1.project.synthlab.port.in.InPort;
 
 import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
+import com.jsyn.ports.UnitInputPort;
 import com.jsyn.scope.AudioScope;
 import com.jsyn.unitgen.LineOut;
 import com.jsyn.unitgen.PassThrough;
@@ -35,14 +36,16 @@ public class OutModule extends Module implements IModule {
 		super("Out-" + moduleCount);
 		lineOut = new LineOut();
 		
-		circuit.add(lineOut);
+		//Ne pas ajouter LineOut au ciruit!
+		//Bugs!
 		
 		passThrough = new PassThrough();
 		setDistribution(Distribution.STEREO);
 
-		//lineOut.input.setup(-5, -4, 5);
+		lineOut.input.setup(-5, -4, 5);
 		lineOut.input.setMaximum(5);
 		lineOut.input.setMinimum(-5);
+	
 
 		leftPort = new InPort("Source Left", passThrough.input);
 		rightPort = new InPort("Source Right", passThrough.input);
@@ -75,17 +78,24 @@ public class OutModule extends Module implements IModule {
 	public InPort getLeftPort() {
 		return leftPort;
 	}
+	
+	public InPort getRightPort() {
+		return rightPort;
+	}
 
 	public Distribution getDistribution() {
 		return distribution;
 	}
 
 	public void start() {
-		lineOut.start();
+		circuit.start();
+		lineOut.setSynthesisEngine(circuit.getSynthesisEngine());
+		lineOut.start();		
 		isOn = true;
 	}
 
 	public void stop() {
+		circuit.stop();
 		lineOut.stop();
 		isOn = false;
 	}
@@ -99,7 +109,6 @@ public class OutModule extends Module implements IModule {
 		// Create a context for the synthesizer.
 		Synthesizer synth = JSyn.createSynthesizer();
 
-		// Start synthesizer using default stereo output at 44100 Hz.
 		synth.start();
 		
 		SineOscillator osc;
@@ -107,24 +116,26 @@ public class OutModule extends Module implements IModule {
 		synth.add( osc = new SineOscillator() );
 		// Add a stereo audio output unit.
 		synth.add(out.getCircuit());
+		osc.output.connect(out.getLeftPort().getJSynPort());
 		// Set the frequency and amplitude for the sine wave.
-		osc.frequency.set(345.0);
-		
+		osc.frequency.set(345.0);		
 		osc.amplitude.set(0.6);
-
-		
-		out.getLeftPort().getJSynPort().connect(osc.output);
+			
 		
 		out.start();
+		
+	
 		
 		 AudioScope scope= new AudioScope( synth );
 		scope.addProbe( osc.output );
 		scope.setTriggerMode( AudioScope.TriggerMode.NORMAL );
 		
-		//scope.getModel().getTriggerModel().getLevelModel().setDoubleValue( 0.0001 );
-		// Turn off the gain and trigger control GUI.
+		scope.getModel().getTriggerModel().getLevelModel().setDoubleValue( 0.0001 );
+	
 		scope.getView().setShowControls( true );
 		scope.start();
+		
+				
 		
 		JFrame frame = new JFrame();
 		frame.add(scope.getView());
