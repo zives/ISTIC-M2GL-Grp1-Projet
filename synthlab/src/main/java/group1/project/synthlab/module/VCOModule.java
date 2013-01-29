e320663223e114a3619b34f80821ad88c5f649d1
Adrien <djitouw@gmail.com>
2013-01-29 14:36:57 +0100

    Suppression messages d'erreurs

--- synthlab/src/main/java/group1/project/synthlab/module/VCOModule.java
+++ synthlab/src/main/java/group1/project/synthlab/module/VCOModule.java
@@ -2,5 +2,20 @@ package group1.project.synthlab.module;
 
 public class VCOModule extends Module {
 
+	public VCOModule(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
---
+++ synthlab/src/test/java/group1/project/synthlab/test/Test.java
@@ -0,0 +1,5 @@
+package group1.project.synthlab.test;
+
+public class Test {
+
+}
