0aba29af37a959685268de4d23692ea0c4996d8f
djitouw <djitouw@gmail.com>
2013-02-02 15:58:47 +0100

    #VCO : Commentaires + Javadoc + Tests unitaires + Tests fonctionnels

--- synthlab/src/main/java/group1/project/synthlab/module/vco/IVCOModule.java
+++ synthlab/src/main/java/group1/project/synthlab/module/vco/IVCOModule.java
@@ -1,24 +1,98 @@
 package group1.project.synthlab.module.vco;
 
+import com.jsyn.unitgen.SineOscillator;
+import com.jsyn.unitgen.SquareOscillator;
+import com.jsyn.unitgen.TriangleOscillator;
+
 import group1.project.synthlab.module.IModule;
+import group1.project.synthlab.port.IPort;
 import group1.project.synthlab.port.in.IInPort;
 import group1.project.synthlab.port.out.IOutPort;
 
 public interface IVCOModule extends IModule {
+	
+	/**
+	 * @return la valeur actuelle de la frequence de base
+	 */
+	public double getf0();
+	
+	/**
+	 * @return l'oscillateur JSyn qui genere le signal sinusoidale
+	 */
+	public SineOscillator getSineOsc();
+
+	/**
+	 * @return l'oscillateur JSyn qui genere le signal carre
+	 */
+	public SquareOscillator getSquareOsc();
+
+	/**
+	 * @return l'oscillateur JSyn qui genere le signal triangulaire
+	 */
+	public TriangleOscillator getTriangleOsc();
+	
+	/**
+	 * @return l'etat de l'entree fm (signal modulant connecte ou non)
+	 */
+	public boolean getFmConnected();
+	
+	/**
+	 * @return le port d'entree Fm
+	 */
 	public IInPort getFm();
 
+	/**
+	 * @return le port de sortie pour le signal sinusoidale
+	 */
 	public IOutPort getOutSine();
 
+	/**
+	 * @return le port de sortie pour le signal carre
+	 */
 	public IOutPort getOutSquare();
 
+	/**
+	 * @return le port de sortie pour le signal triangulaire
+	 */
 	public IOutPort getOutTriangle();
 	
-	public int getOctave();
+	/**
+	 * Reglage grossier (dixieme de la plage de frequences)
+	 * @return la valeur du reglage grossier de la frequence f0 (entier de 0 a 9)
+	 */
+	public int getCoarseAdjustment();
 
-	public void setOctave(int reglageoctave);
+	/**
+	 * @param coarseadjustment : la nouvelle valeur du reglage grossier de la frequence f0 (entier de 0 a 9)
+	 */
+	public void setCoarseAdjustment(int coarseadjustment);
 
+	/**
+	 * Reglage fin
+	 * @return la valeur du reglage fin de la frequence f0 (double entre 0 et 1)
+	 */
 	public double getFineAdjustment();
 
-	public void setFineAdjustment(double reglagefin);
+	/**
+	 * @param fineadjustment : la nouvelle valeur du reglage fin de la frequence f0 (double entre 0 et 1)
+	 */
+	public void setFineAdjustment(double fineadjustment);
+	
+	/**
+	 * Met a jour la frequence f0 des 3 signaux en fonction des valeurs de reglage grossier et fin
+	 */
+	public void changeFrequency();
+	
+	/**
+	 * Si un cable est connecte dans le port d'entree fm, le VCO doit activer la modulation de frequence
+	 * @param port : le port du VCO auquel on vient de connecter un cable
+	 */
+	public void cableConnected(IPort port);
+	
+	/**
+	 * Si un cable est deconnecte du port d'entree fm, le VCO doit desactiver la modulation de frequence et remettre les 3 signaux a la frequence f0
+	 * @param port : le port du VCO duquel on vient de deconnecter un cable
+	 */
+	public void cableDisconnected(IPort port);
 
 }
---
+++ synthlab/src/test/java/group1/project/synthlab/test/TestVCOModule.java
@@ -0,0 +1,113 @@
+package group1.project.synthlab.test;
+
+
+import group1.project.synthlab.factory.Factory;
+import group1.project.synthlab.module.vco.IVCOModule;
+import group1.project.synthlab.module.vco.VCOModule;
+import junit.framework.TestCase;
+
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.unitgen.Circuit;
+
+public class TestVCOModule extends TestCase {
+	
+	Circuit c;
+
+	protected IVCOModule vco;
+	protected Synthesizer synth;
+	
+	protected void setUp() throws Exception {
+		Factory factory = new Factory();
+		super.setUp();
+
+		vco = factory.createVCOModule();
+		
+		synth = JSyn.createSynthesizer();
+		synth.add(vco.getCircuit());
+
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#VCOModule(group1.project.synthlab.factory.Factory)}.
+	 */
+	public void testVCOModule() {
+		assert (vco.getSineOsc() != null);
+		assert (vco.getSquareOsc() != null);
+		assert (vco.getTriangleOsc() != null);
+		assert (vco.getFm() != null);
+		assert (vco.getCircuit() != null);
+		assert (vco.getOutSine() != null);
+		assert (vco.getOutSquare() != null);
+		assert (vco.getOutTriangle() != null);
+		assert (vco.getOutSine() != null);
+		assert (vco.getFmConnected() == false);
+		assert (vco.getSineOsc().amplitude.get() == VCOModule.a0);
+		assert (vco.getSquareOsc().amplitude.get() == VCOModule.a0);
+		assert (vco.getTriangleOsc().amplitude.get() == VCOModule.a0);
+		assert (vco.getSineOsc().frequency.get() == vco.getf0());
+		assert (vco.getSquareOsc().frequency.get() == vco.getf0());
+		assert (vco.getTriangleOsc().frequency.get() == vco.getf0());
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#changeFrequency()}.
+	 */
+	public void testChangeFrequency() {
+		vco.setCoarseAdjustment(1);
+		vco.setFineAdjustment(0.2);
+		vco.changeFrequency();
+		assert (vco.getSineOsc().frequency.get() == (VCOModule.fmax - VCOModule.fmin)/50);
+		assert (vco.getSquareOsc().frequency.get() == (VCOModule.fmax - VCOModule.fmin)/50);
+		assert (vco.getTriangleOsc().frequency.get() == (VCOModule.fmax - VCOModule.fmin)/50);
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#cableConnected(group1.project.synthlab.port.IPort)}.
+	 */
+	public void testCableConnected() {
+		vco.cableConnected(vco.getFm());
+		assert (vco.getFmConnected() == true);
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#cableDisconnected(group1.project.synthlab.port.IPort)}.
+	 */
+	public void testCableDisconnected() {
+		vco.cableDisconnected(vco.getFm());
+		assert (vco.getFmConnected() == false);
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#start()}.
+	 */
+	public void testStart() {
+		vco.start();
+		assert(vco.isStarted());
+		assert (vco.getSineOsc().amplitude.get() == VCOModule.a0);
+		assert (vco.getSquareOsc().amplitude.get() == VCOModule.a0);
+		assert (vco.getTriangleOsc().amplitude.get() == VCOModule.a0);
+		System.err.println("Start...");
+	}
+
+	/**
+	 * Test method for {@link group1.project.synthlab.module.vco.VCOModule#stop()}.
+	 */
+	public void testStop() {
+		System.err.println("Preparation au stop...");
+		vco.start();
+		vco.stop();
+		assert(!vco.isStarted());
+		assert (vco.getSineOsc().amplitude.get() == VCOModule.amin);
+		assert (vco.getSquareOsc().amplitude.get() == VCOModule.amin);
+		assert (vco.getTriangleOsc().amplitude.get() == VCOModule.amin);
+		System.err.println("Stop...");
+		
+		synth.stop();
+	}
+
+}
