673758b467f28a07e41764beb51e4b3b25aa013e
zives <d.zives@gmail.com>
2013-02-11 14:41:19 +0100

    modifications graphiques

--- synthlabGUI/src/main/java/group1/project/synthlab/ihm/module/multiplexer/PMultiplexerModule.java
+++ synthlabGUI/src/main/java/group1/project/synthlab/ihm/module/multiplexer/PMultiplexerModule.java
@@ -7,7 +7,10 @@ import group1.project.synthlab.ihm.port.out.ICOutPort;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -122,7 +125,45 @@ public class PMultiplexerModule extends PModule implements IPMultiplexerModule {
 		pIn4Slider.setMajorTickSpacing(100);
 		pIn4Slider.setMinorTickSpacing(100);
 		pIn4Slider.setPaintTicks(true);
-
+		
+		// Label volume
+		final JLabel volumeLabel1 = new JLabel("0 dB" ,JLabel.CENTER /*controller.getAttenuation() + " dB"*/);
+		volumeLabel1.setForeground(Color.LIGHT_GRAY);
+		volumeLabel1.setOpaque(false);
+		volumeLabel1.setSize(60, 30);
+		volumeLabel1.setBorder(null);
+		volumeLabel1.setPreferredSize(volumeLabel1.getSize());
+		volumeLabel1.setLocation(pIn1Slider.getX() + pIn1Slider.getWidth() / 2 - volumeLabel1.getWidth() / 2, pIn1Slider.getY() + 22);
+		volumeLabel1.setFont(new Font("Monospaced", Font.ITALIC, 12));
+		
+		final JLabel volumeLabel2 = new JLabel("0 dB" ,JLabel.CENTER /*controller.getAttenuation() + " dB"*/);
+		volumeLabel2.setForeground(Color.LIGHT_GRAY);
+		volumeLabel2.setOpaque(false);
+		volumeLabel2.setSize(60, 30);
+		volumeLabel2.setBorder(null);
+		volumeLabel2.setPreferredSize(volumeLabel2.getSize());
+		volumeLabel2.setLocation(pIn2Slider.getX() + pIn2Slider.getWidth() / 2 - volumeLabel2.getWidth() / 2, pIn2Slider.getY() + 22);
+		volumeLabel2.setFont(new Font("Monospaced", Font.ITALIC, 12));
+		
+		
+		final JLabel volumeLabel3 = new JLabel("0 dB" ,JLabel.CENTER /*controller.getAttenuation() + " dB"*/);
+		volumeLabel3.setForeground(Color.LIGHT_GRAY);
+		volumeLabel3.setOpaque(false);
+		volumeLabel3.setSize(60, 30);
+		volumeLabel3.setBorder(null);
+		volumeLabel3.setPreferredSize(volumeLabel3.getSize());
+		volumeLabel3.setLocation(pIn3Slider.getX() + pIn3Slider.getWidth() / 2 - volumeLabel3.getWidth() / 2, pIn3Slider.getY() + 22);
+		volumeLabel3.setFont(new Font("Monospaced", Font.ITALIC, 12));
+		
+		final JLabel volumeLabel4 = new JLabel("0 dB" ,JLabel.CENTER /*controller.getAttenuation() + " dB"*/);
+		volumeLabel4.setForeground(Color.LIGHT_GRAY);
+		volumeLabel4.setOpaque(false);
+		volumeLabel4.setSize(60, 30);
+		volumeLabel4.setBorder(null);
+		volumeLabel4.setPreferredSize(volumeLabel4.getSize());
+		volumeLabel4.setLocation(pIn4Slider.getX() + pIn4Slider.getWidth() / 2 - volumeLabel4.getWidth() / 2, pIn4Slider.getY() + 22);
+		volumeLabel4.setFont(new Font("Monospaced", Font.ITALIC, 12));
+		
 		add(pportIn1);
 		add(pportIn2);
 		add(pportIn3);
@@ -135,18 +176,23 @@ public class PMultiplexerModule extends PModule implements IPMultiplexerModule {
 		add(pIn2Slider);
 		add(pIn3Slider);
 		add(pIn4Slider);
+		add(volumeLabel1);
+		add(volumeLabel2);
+		add(volumeLabel3);
+		add(volumeLabel4);
 
-		setSize(getWidth(), getHeight() - 90);
+		setSize(getWidth(), getHeight() - 70);
 		setPreferredSize(getSize());
 		onOffButton.setSelected(true);
 		onOffButton.setText("Off");
+		
 
 		pIn1Slider.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				controller.setAttenuation(pIn1Slider.getValue() / 10.0, 0);
-
+				volumeLabel1.setText(pIn1Slider.getValue() / 10.0 + " dB");
 			}
 		});
 		pIn2Slider.addChangeListener(new ChangeListener() {
@@ -154,6 +200,7 @@ public class PMultiplexerModule extends PModule implements IPMultiplexerModule {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				controller.setAttenuation(pIn2Slider.getValue() / 10.0, 1);
+				volumeLabel2.setText(pIn2Slider.getValue() / 10.0 + " dB");
 
 			}
 		});
@@ -162,6 +209,7 @@ public class PMultiplexerModule extends PModule implements IPMultiplexerModule {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				controller.setAttenuation(pIn3Slider.getValue() / 10.0, 2);
+				volumeLabel3.setText(pIn3Slider.getValue() / 10.0 + " dB");
 
 			}
 		});
@@ -170,6 +218,7 @@ public class PMultiplexerModule extends PModule implements IPMultiplexerModule {
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				controller.setAttenuation(pIn4Slider.getValue() / 10.0, 3);
+				volumeLabel4.setText(pIn4Slider.getValue() / 10.0 + " dB");
 
 			}
 		});
